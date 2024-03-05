dc45c11ef4793e9b61aeea9c5c50d8588565f3d8
Ildar <misi228@mail.ru>
2024-03-05 17:23:56 +0500

    change firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD9HO1oiyvRRJYbcxrzWvVIZsiqa-jw2z0",
@@ -15,7 +16,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
